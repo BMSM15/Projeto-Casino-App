f589060fffa72ffd6e8a724006d57441560b7455
Bruno Martins <brunomiguelmartins1522@gmail.com>
2022-06-08 09:11:15 +0100

    Criação das primeiras paginas

--- app/src/main/java/com/example/projetopa/MainActivity.kt
+++ app/src/main/java/com/example/projetopa/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.projetopa
 
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun startdbapp(view: View) {
+
+
+
     }
 }
