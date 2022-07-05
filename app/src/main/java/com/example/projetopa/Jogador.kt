c4cc5d7d7960ad9d89d1aeee01d3825ce029ffe6
Bruno Martins <brunomiguelmartins1522@gmail.com>
2022-07-05 21:39:37 +0100

    Modificado o codigo da class jogador usando agora a biblioteca Seriazable

--- app/src/main/java/com/example/projetopa/Jogador.kt
+++ app/src/main/java/com/example/projetopa/Jogador.kt
@@ -3,12 +3,13 @@ package com.example.projetopa
 import android.content.ContentValues
 import android.database.Cursor
 import android.provider.BaseColumns
+import java.io.Serializable
 
 data class Jogador(
-    var Nome_jogador : String,
-    var Dinheiro: Int,
+    var Nome_jogador: String,
+    var Dinheiro: String,
     var id: Long = -1
-) {
+) : Serializable {
     fun toContentValues() : ContentValues {
         val valores = ContentValues()
 
@@ -25,7 +26,7 @@ data class Jogador(
 
             val id = cursor.getLong(posId)
             val Nome_jogador = cursor.getString(posNomeJogador)
-            val Dinheiro = cursor.getInt(posDinheiro)
+            val Dinheiro = cursor.getString(posDinheiro)
 
             return Jogador(Nome_jogador, Dinheiro, id)
         }
